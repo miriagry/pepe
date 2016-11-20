0991b2888f01e5f1908014302f0dfe6e60fcdd92
miriagry <miriagry@gmail.com>
2016-11-20 19:17:40 +0300

    start

--- pepe/settings.py
+++ pepe/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^a6gj!3-r)f)*2k2$tc7%9siei5)kw8lpz)5vvsgja=n&npz7%'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
