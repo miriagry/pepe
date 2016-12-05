1f22cfaa3e971acbefb2ace4227950ba0ab928f1
miriagry <miriagry@gmail.com>
2016-12-05 23:51:22 +0300

    rnd quote out!

--- inliner/book_to_model.py
+++ inliner/book_to_model.py
@@ -1,4 +1,5 @@
 from lxml import etree
+import json
 
 tree = etree.parse('warcraft_shorten.xml')
 root = tree.getroot()
@@ -6,10 +7,31 @@ clear = tree
 for link in clear.findall('//a'):
     if link.tail != None: link.getparent().text += link.tail
     link.getparent().remove(link)
+
+
+#print etree.tostring(clear, pretty_print = 'True', encoding = 'utf-8')
+
+dumpdata = []
+pk = 1
 for section in clear.getroot()[0]:
-    print section[0][0].text, section[0][1].text
+    chapter = section[0][0].text #section[0][1].text
     for subsection in section[1:]:
-        print subsection[0][0].text
+        stringNum = subsection[0][0].text
+        stringText = ''
         for each in subsection[1:]:
-            print each.text
-print etree.tostring(clear, pretty_print = 'True', encoding = 'utf-8')
+            stringText += each.text
+        dumpdata.append({
+            "model":"inliner.Quote",
+            "pk":pk,
+            "fields":
+                {
+                    "text":stringText,
+                    "chapternum":chapter[:-1],
+                    "stringnum":stringNum[:-1],
+                }
+        })
+        pk += 1
+
+target = open('fixtures/datata.json', 'w')
+target.write(json.dumps(dumpdata))
+target.close()
