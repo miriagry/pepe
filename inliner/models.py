8d5de8f386073473611a1608814ba20c93158874
miriagry <miriagry@gmail.com>
2016-12-05 23:50:38 +0300

    first models added

--- inliner/models.py
+++ inliner/models.py
@@ -1,10 +1,20 @@
 from __future__ import unicode_literals
-
+from django.db.models.aggregates import Count
+from random import randint
 from django.db import models
 
 class Quote (models.Model):
     text = models.CharField(max_length=2000, default='placeholder')
     repostcounter = models.IntegerField(default=0)
+    stringnum = models.CharField(max_length=3, default='000')
+    chapternum = models.CharField(max_length=3, default='000')
+    def __str__(self):
+        return '%s - %s' % (self.chapternum, self.stringnum)
+    def random(self):
+        count = self.aggregate(count=Count('id'))['count']
+        random_index = randint(0, count - 1)
+        return self.all()[random_index]
+
 
 class Book (models.Model):
     name = models.CharField(max_length=50, default='name')
--- inliner/views.py
+++ inliner/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from random import randint
+from .models import Quote
 
 def index(request):
-    return HttpResponse("yo wuzzup")
+    counter = Quote.objects.all().count()
+    ri = randint(0, counter - 1)
+    context = {'quote' : Quote.objects.all()[ri]}
+    return render(request, 'random_quote.html', context)
 # Create your views here.
